fce5bd0672550db3de7785cfba5c6fefa71ac798
Chiaki203 <42350913+Chiaki203@users.noreply.github.com>
2026-03-21 16:41:33 +0100

    fix cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  baseUrl: process.env.CYPRESS_BASE_URL ?? 'http://localhost:4000',
   e2e: {
+    baseUrl: process.env.CYPRESS_BASE_URL ?? 'http://localhost:4000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
